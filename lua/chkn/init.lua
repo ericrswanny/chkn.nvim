3d79f1e67f7b571d98a188fb10eefd0b63d15f10
Eric Swanson <es20641@gmail.com>
2024-11-28 11:03:14 -0600

    Remove print statements

--- lua/chkn/init.lua
+++ lua/chkn/init.lua
@@ -1,7 +1,6 @@
 local M = {}
 
 function M.setup(user_config)
-	print("chkn.nvim setup called")
 	require("chkn.scratchpad").setup(user_config)
 end
 
--- plugin/chkn.lua
+++ plugin/chkn.lua
@@ -1,4 +1,3 @@
 vim.api.nvim_create_user_command("ChknToggle", function()
-	print("ChknToggle command is being loaded")
 	require("chkn.scratchpad").open()
 end, { desc = "Toggle Chkn" })
